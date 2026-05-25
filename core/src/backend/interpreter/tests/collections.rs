fb308084f0c3dbe468a91e734cc829b31f8cde09
Ueslei Paim <uesleibros@gmail.com>
2026-05-24 21:34:50 -0300

    added namespaces and removed dead code

---
+++ core/src/backend/interpreter/tests/collections.rs
@@ -0,0 +1,102 @@
+use crate::run_source;
+
+#[test]
+fn list_works() {
+    let source = r#"
+Import Valo.Collections
+
+Sub Main()
+    Dim l As New List(Of String)()
+    l.Add("Hello")
+    l.Add("World")
+    Console.WriteLine(l.Count)
+    Console.WriteLine(l(0))
+    Console.WriteLine(l(1))
+End Sub
+"#;
+    let output = run_source(source).unwrap();
+    assert_eq!(output, vec!["2", "Hello", "World"]);
+}
+
+#[test]
+fn dictionary_works() {
+    let source = r#"
+Import Valo.Collections
+
+Sub Main()
+    Dim d As New Dictionary(Of String, Integer)()
+    d.Add("A", 1)
+    d.Add("B", 2)
+    Console.WriteLine(d.Count)
+    Console.WriteLine(d("A"))
+    Console.WriteLine(d("B"))
+    Console.WriteLine(d.Exists("A"))
+    Console.WriteLine(d.ContainsKey("C"))
+End Sub
+"#;
+    let output = run_source(source).unwrap();
+    assert_eq!(output, vec!["2", "1", "2", "True", "False"]);
+}
+
+#[test]
+fn collection_iteration_works() {
+    let source = r#"
+Import Valo.Collections
+
+Sub Main()
+    Dim l As New List(Of Integer)()
+    l.Add(10)
+    l.Add(20)
+    Dim total As Integer
+    Dim item As Variant
+    For Each item In l
+        total = total + item
+    Next
+    Console.WriteLine(total)
+
+    Dim d As New Dictionary(Of String, String)()
+    d.Add("K1", "V1")
+    d.Add("K2", "V2")
+    Dim keys As String
+    For Each item In d
+        keys = keys & item & " "
+    Next
+    Console.WriteLine(keys)
+    Console.WriteLine("Done")
+End Sub
+"#;
+    let output = run_source(source).unwrap();
+    assert_eq!(output, vec!["30", "K1 K2 ", "Done"]);
+}
+
+#[test]
+fn collection_type_safety_fails() {
+    let source = r#"
+Import Valo.Collections
+
+Sub Main()
+    Dim l As New List(Of Integer)()
+    l.Add("Not an integer")
+End Sub
+"#;
+    let result = run_source(source);
+    assert!(result.is_err());
+    let err = result.unwrap_err();
+    assert!(
+        err.message
+            .contains("Cannot assign String value to Integer variable")
+            || err.message.contains("Type mismatch")
+    );
+}
+
+#[test]
+fn collection_unimported_fails() {
+    let source = r#"
+Sub Main()
+    Dim l As New List(Of Integer)()
+End Sub
+"#;
+    let result = run_source(source);
+    assert!(result.is_err());
+    assert!(result.unwrap_err().message.contains("is not defined"));
+}
---
+++ core/src/backend/interpreter/tests/namespaces.rs
@@ -0,0 +1,138 @@
+use super::modules::{temp_project, write};
+use crate::run_file;
+use std::fs;
+
+#[test]
+fn class_in_namespace_different_from_module_name() {
+    let dir = temp_project();
+    write(
+        &dir,
+        "main.valo",
+        r#"
+Import LibFile
+
+Sub Main()
+    Dim s As New Game.Graphics.Sprite()
+    Console.WriteLine("Success")
+End Sub
+"#,
+    );
+    write(
+        &dir,
+        "LibFile.valo",
+        r#"
+Namespace Game.Graphics
+Public Class Sprite
+End Class
+End Namespace
+"#,
+    );
+
+    let result = run_file(dir.join("main.valo"));
+    match result {
+        Ok(output) => assert_eq!(output, vec!["Success"]),
+        Err(err) => panic!("Should have succeeded, but got error: {}", err),
+    }
+    fs::remove_dir_all(dir).unwrap();
+}
+
+#[test]
+fn namespace_import_works() {
+    let dir = temp_project();
+    write(
+        &dir,
+        "main.valo",
+        r#"
+Import LibFile
+Import Game.Graphics
+
+Sub Main()
+    Dim s As New Sprite()
+    Console.WriteLine("Success")
+End Sub
+"#,
+    );
+    write(
+        &dir,
+        "LibFile.valo",
+        r#"
+Namespace Game.Graphics
+Public Class Sprite
+End Class
+End Namespace
+"#,
+    );
+
+    let result = run_file(dir.join("main.valo"));
+    match result {
+        Ok(output) => assert_eq!(output, vec!["Success"]),
+        Err(err) => panic!("Should have succeeded, but got error: {}", err),
+    }
+    fs::remove_dir_all(dir).unwrap();
+}
+
+#[test]
+fn namespace_alias_import_works() {
+    let dir = temp_project();
+    write(
+        &dir,
+        "main.valo",
+        r#"
+Import LibFile
+Import Game.Graphics As G
+
+Sub Main()
+    Dim s As New G.Sprite()
+    Console.WriteLine("Success")
+End Sub
+"#,
+    );
+    write(
+        &dir,
+        "LibFile.valo",
+        r#"
+Namespace Game.Graphics
+Public Class Sprite
+End Class
+End Namespace
+"#,
+    );
+
+    let result = run_file(dir.join("main.valo"));
+    match result {
+        Ok(output) => assert_eq!(output, vec!["Success"]),
+        Err(err) => panic!("Should have succeeded, but got error: {}", err),
+    }
+    fs::remove_dir_all(dir).unwrap();
+}
+
+#[test]
+fn private_namespace_member_rejected() {
+    let dir = temp_project();
+    write(
+        &dir,
+        "main.valo",
+        r#"
+Import LibFile
+
+Sub Main()
+    Dim s As New Game.Graphics.PrivateSprite()
+End Sub
+"#,
+    );
+    write(
+        &dir,
+        "LibFile.valo",
+        r#"
+Namespace Game.Graphics
+Private Class PrivateSprite
+End Class
+End Namespace
+"#,
+    );
+
+    let result = run_file(dir.join("main.valo"));
+    assert!(result.is_err());
+    assert!(result.unwrap_err().contains("is Private"));
+    fs::remove_dir_all(dir).unwrap();
+}
